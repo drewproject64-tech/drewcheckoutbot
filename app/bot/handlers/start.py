696bbf897f2d6878a0c74df15680f58f5518e0a7
drewproject64-tech <drewproject64@gmail.com>
2026-09-06 09:23:51 +0100

    fix: use middleware-provided user in start handler

--- app/bot/handlers/start.py
+++ app/bot/handlers/start.py
@@ -3,14 +3,11 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 
 from app.bot.keyboards.navigation import language_keyboard
-from app.database.repositories import upsert_user
 from app.locales import get_text
 
 router = Router(name="start")
 
 
 @router.message(CommandStart())
-async def start(message: Message, db_session):
-    user = await upsert_user(db_session.session, message.from_user)
-    await db_session.session.commit()
+async def start(message: Message, user):
     await message.answer(get_text("welcome", user.language), reply_markup=language_keyboard())
